337d80152133f56f42749e0f738305a0fe0ccd94
Aaron <aaron1515@gmail.com>
2017-10-30 20:25:51 -0700

    add link to about and contact on nav bar

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,2 +1,14 @@
 class AboutController < ApplicationController
+
+	def what_we_offer
+		
+	end
+
+	def who_we_are
+
+	end
+
+	def in_the_news
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
+  get 'about/what_we_offer'
+  get 'about/who_we_are'
+  get	'about/in_the_news'
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

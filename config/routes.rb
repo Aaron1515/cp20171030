42279c3ff5b7a6e06e69d4ca0d7014a6a88dec4e
Aaron <aaron1515@gmail.com>
2017-11-03 12:13:15 -0700

    add blank pages, routes, controller for fundamentals

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,16 @@ Rails.application.routes.draw do
   get	'about/faq'
 
   get 'lesson/index'
+
   get 'lesson/fundamental'
+  get 'lesson/fundamental_gpio'
+  get 'lesson/fundamental_loops'
+  get 'lesson/fundamental_comparison'
+  get 'lesson/fundamental_conditional'
+  get 'lesson/fundamental_logic'
+
   get 'lesson/self_driving_car'
+
   get 'lesson/robot_car'
   get 'lesson/robot_car_h_bridge'
   get 'lesson/robot_car_motor'

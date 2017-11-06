6a5f700242350f66edfb63ff6c4e70c1be7ffc53
Aaron <aaron1515@gmail.com>
2017-11-06 13:59:38 -0800

    add files for robot car create route for download robot car files created link to download robot car files edit robot car files, removed dead spaces tested download link, working to download files for robot car

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,2 +1,7 @@
 class DownloadController < ApplicationController
+
+	def robot_car
+		send_file "#{Rails.root}/app/assets/downloads/robot_car.zip", type: "application/zip", x_sendfile: true
+	end
+
 end

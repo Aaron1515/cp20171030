0030ee2a269020e37353681716b938a5f33114d7
Aaron <aaron1515@gmail.com>
2017-11-08 11:20:26 -0800

    update fundamenats.zip from fundamantal.zip

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -5,6 +5,6 @@ class DownloadController < ApplicationController
 	end
 
 	def fundamental
-		send_file "#{Rails.root}/app/assets/downloads/fundamental.zip", type: "application/zip", x_sendfile: true
+		send_file "#{Rails.root}/app/assets/downloads/fundamentals.zip", type: "application/zip", x_sendfile: true
 	end
 end

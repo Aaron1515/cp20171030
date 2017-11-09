bef57365d2974900eed89abacce921b6f20a9385
Aaron <aaron1515@gmail.com>
2017-11-08 19:28:01 -0800

    add pop up image to gpio

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,11 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(function() {
+  	$('.pop-img').on('click', function() {
+		$('.enlargeImageModalSource').attr('src', $(this).attr('src'));
+		$('#enlargeImageModal').modal('show');
+	});
+});

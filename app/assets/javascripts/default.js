3880fc4b19b8633f8309f9d87c9199c24529296d
Aaron <aaron1515@gmail.com>
2017-11-16 15:03:49 -0800

    removed js for bread crumb refactor sticky shorten breadcrumb add box shaadow to breadcrumb

--- app/assets/javascripts/default.js
+++ app/assets/javascripts/default.js
@@ -1,14 +1,14 @@
 // Get the breadcrumb
-var breadCrumb = document.getElementById("breadcrumb");
+// var breadCrumb = document.getElementById("breadcrumb");
 
 // Get the offset position of the breadCrumb
-var sticky = breadCrumb.offsetTop;
+// var sticky = breadCrumb.offsetTop;
 
 // Add the sticky class to the breadCrumb when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    breadCrumb.classList.add("sticky")
-  } else {
-    breadCrumb.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     breadCrumb.classList.add("sticky")
+//   } else {
+//     breadCrumb.classList.remove("sticky");
+//   }
+// }

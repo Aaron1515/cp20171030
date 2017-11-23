3e7ca3de3cbae58e1aa7db47f2de3d0c4fd48815
Aaron <aaron1515@gmail.com>
2017-11-22 21:09:08 -0800

    add basic test to model

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Activity, type: :model do
+RSpec.describe Subscriber, type: :model do
+
   it "has none to begin with" do
-  	expect(Activity.count).to eq 0
+  	expect(Subscriber.count).to eq 0
   end
 
   it "has one after adding one" do
-    Activity.create
-    expect(Activity.count).to eq 1
+    Subscriber.create
+    expect(Subscriber.count).to eq 1
   end
 
   it "has none after one was created in a previous example" do
-    expect(Activity.count).to eq 0
+    expect(Subscriber.count).to eq 0
   end
+
 end

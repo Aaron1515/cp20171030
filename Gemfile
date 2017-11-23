76d86e5cd60e21f25932ebd91d4dc26afd383a23
Aaron <aaron1515@gmail.com>
2017-11-22 22:59:56 -0800

    add guard and pry gems

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem "rspec-rails"
+  gem "guard"
+  gem "pry-rails"
 
 end
 
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -34,18 +34,18 @@ RSpec.describe Subscriber, type: :model do
 
 
 
-	 #  it "has none to begin with" do
-	 #  	expect(Subscriber.count).to eq 0
-	 #  end
+	  it "has none to begin with" do
+	  	expect(Subscriber.count).to eq 0
+	  end
 
-	 #  it "has one after adding one" do
-	 #    subject_3
-	 #    expect(Subscriber.count).to eq 1
-	 #  end
+	  it "has one after adding one" do
+	    subject_5
+	    expect(Subscriber.count).to eq 1
+	  end
 
-	 #  it "has none after one was created in a previous example" do
-	 #    expect(Subscriber.count).to eq 0
-	 #  end
+	  it "has none after one was created in a previous example" do
+	    expect(Subscriber.count).to eq 0
+	  end
 
 
 	 #  it "is valid with valid attributes" do

371e76879a053566b07ace366e00c60cf79d9666
Aaron <aaron1515@gmail.com>
2017-11-22 22:45:51 -0800

    crate 18 valid test with 3 subject

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,6 @@ class Activity < ApplicationRecord
 
 	has_and_belongs_to_many :subscribers
 
+	validates :note, :presence => true
+
 end
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -2,16 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
 
+	let(:subject_1) { Activity.new }
+	let(:subject_2) { Activity.new(note: "success") }
+	let(:subject_3)	{ Activity.create(note: "successful") }
+
 
 	describe "Basic Model Test" do
+
+		it "test model 1 to be invalid" do
+			expect(subject_1).to be_invalid
+		end
+
+		it "test model 2 to be valid" do
+			expect(subject_2).to be_valid
+		end
+
+		it "test model 3 to be valid" do
+			expect(subject_3).to be_valid
+		end
+
+
 
 	  it "has none to begin with" do
 	  	expect(Activity.count).to eq 0
 	  end
 
-
 	  it "has one after adding one" do
-	    Activity.create
+	    subject_3
 	    expect(Activity.count).to eq 1
 	  end
 
@@ -19,38 +36,56 @@ RSpec.describe Activity, type: :model do
 	    expect(Activity.count).to eq 0
 	  end
 
+
+	  it "is valid with valid attributes" do
+	    subject_2
+	    expect(subject_2).to be_valid
+	  end
+
+		it "is not valid without note" do
+	    expect(subject).to_not be_valid
+	  end
+
 	  it "create a valid entry" do
-	    work = Activity.create
+	    work = subject_3
 	    expect(work).to be_valid
 	  end
 
 	  it "save 1 entry" do
-	    work = Activity.new
-	    work.save
+	    subject_2.save
 	    expect(Activity.count).to eq 1
 	  end
 
 	  it "save 2 entry" do
-	    work1 = Activity.new
-	    work1.save
-	    work2 = Activity.new
-	    work2.save
+	    subject_1.note = "success"
+	    subject_1.save
+	    work = Activity.new(note: "test2")
+	    work.save
 	    expect(Activity.count).to eq 2
 	  end
 
 	  it "save 1 entry & create 1 entry" do
-	    work1 = Activity.new
-	    work1.save
+	    subject_1.note = "success"
+	    subject_1.save
 	    work2 = Activity.create(note: "John successfully logged in.")
 	    expect(Activity.count).to eq 2
 	  end
 
-	  it "expect 1st entry's note to have equal 'success' " do
-	    work = Activity.create(note: "success")
-	    expect(Activity.all.first.note).to eq("success")
+	  it "create 2 entry, only saving 1" do
+			subject_1.save
+			subject_3
+	    expect(Activity.count).to eq 1
 	  end
 
+	  it "expect 1st entry's note to have equal 'success' " do
+	    subject_3
+	    expect(Activity.all.first.note).to eq("successful")
+	  end
+
+
 	end
+
+
 
 	describe "Association Test" do
 

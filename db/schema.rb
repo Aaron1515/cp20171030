cd0e234811116f6fdbc6a235cdf449cfcf66b7c7
Aaron <aaron1515@gmail.com>
2017-12-03 20:49:04 -0800

    update data base

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129034740) do
+ActiveRecord::Schema.define(version: 20171204023613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,11 +25,14 @@ ActiveRecord::Schema.define(version: 20171129034740) do
   end
 
   create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "organization"
-    t.text   "comment"
-    t.string "status"
+    t.string   "name"
+    t.string   "email"
+    t.string   "organization"
+    t.string   "status"
+    t.text     "comment"
+    t.text     "note"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "subscribers", force: :cascade do |t|

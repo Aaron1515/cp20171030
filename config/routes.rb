b11fc68f369a8f8b155613dd40eb2bd17777f563
Aaron <aaron1515@gmail.com>
2017-12-03 20:56:02 -0800

    update routes for contacts

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   get 'welcome/a_test'
   get 'welcome/v_test'
 
-  get 'contact/index'
-
   get 'resource/index'
   get 'resource/guide'
   get 'resource/trouble'
@@ -41,6 +39,7 @@ Rails.application.routes.draw do
 
 
   resources :subscribers
+  resources :contacts
 
   root 'welcome#index'
 

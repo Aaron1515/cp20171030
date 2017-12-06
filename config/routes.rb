ba7ef74c317a84d79ad89ad3c4ba1c329f7f501e
Aaron <aaron1515@gmail.com>
2017-12-05 23:39:18 -0800

    removed admin sing up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
+  devise_for :users
+  devise_for :admins, :skip => [:registrations] 
+    as :admin do
+      get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+      put 'admins' => 'devise/registrations#update', :as => 'admin_registration'
+    end
   resources :subscribers
   resources :contacts
   
